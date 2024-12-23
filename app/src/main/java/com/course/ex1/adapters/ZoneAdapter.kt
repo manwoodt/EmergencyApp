350b064e3ad6964522d3e91d2c863875d70f06ca
manwoodtWindows <manwoodt@gmail.com>
2024-12-24 00:51:02 +0300

    add binding in adapter

--- app/src/main/java/com/course/ex1/adapters/ZoneAdapter.kt
+++ app/src/main/java/com/course/ex1/adapters/ZoneAdapter.kt
@@ -2,11 +2,9 @@ package com.course.ex1.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.view.View
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.course.domain.model.Zone
-import com.course.ex1.R
+import com.course.ex1.databinding.ItemZoneBinding
 
 class ZoneAdapter() :
     RecyclerView.Adapter<ZoneAdapter.ViewHolder>() {
@@ -14,19 +12,16 @@ class ZoneAdapter() :
     private val zoneList = mutableListOf<Zone>()
     var onZoneClick: ((Zone) -> Unit)? = null
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val nameTextView: TextView = view.findViewById(R.id.zone_name)
-        val colorIndicator: View = view.findViewById(R.id.zone_color)
+    class ViewHolder(val binding: ItemZoneBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(zone: Zone, onZoneClick: (Zone) -> Unit) {
-            nameTextView.text = zone.name
-            colorIndicator.setBackgroundColor(zone.level)
+            binding.zoneName.text = zone.name
+            binding.zoneColor.setBackgroundColor(zone.level)
             itemView.setOnClickListener { onZoneClick(zone) }
 
         }
     }
 
-    // Функция для обновления данных
     fun setZones(zones: List<Zone>) {
         zoneList.clear()
         zoneList.addAll(zones)
@@ -34,15 +29,13 @@ class ZoneAdapter() :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_zone, parent, false)
-        return ViewHolder(view)
+        val binding = ItemZoneBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val zone = zoneList[position]
         holder.bind(zone) {
-            // Обработка клика на элемент
             onZoneClick?.invoke(it)
         }
     }
